3bcbc5be73839e9036d0bf129110eeee9a5c0391
jung-han <hanjung@nhnent.com>
2020-06-14 20:41:02 +0900

    feat: add redux setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,34 @@
-import React, { ReactNode } from "react";
+import React, { FC } from "react";
 import { AppProps } from "next/app";
 import "../common/styles/reset.css";
 import Layout from "../common/styles/Layout";
+import { createWrapper, MakeStore } from "next-redux-wrapper";
+import reducer from "../reducers";
+import { createStore, compose, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-// type AppProps = {
-//   Component: ReactNode;
-//   pageProps: any
-// };
+export interface State {
+  user: null;
+}
 
-function App({ Component, pageProps }: AppProps) {
-  console.log("pageProps: ", pageProps);
-  console.log("Component: ", Component);
+const makeStore: MakeStore<State> = () => {
+  const middlewares: any = [];
+  const enhancer =
+    process.env.NODE_ENV === "production" // eslint-disable-line no-undef
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+
+  return createStore(reducer, {}, enhancer);
+};
+
+const wrapper = createWrapper(makeStore, { debug: true });
+
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
-}
+};
 
-export default App;
+export default wrapper.withRedux(WrappedApp);
