f16d77b1fd3ac957c083ae38aa27de3ca7406252
jung-han <hanjung@nhnent.com>
2020-07-12 20:22:25 +0900

    feat: remove unused code

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,16 +1,10 @@
 import { combineReducers } from "redux";
-import { countReducer, ICountState } from "./count";
 import todo from "./todo";
 import user from "./user";
 import style from "./style";
 
-export type IStore = {
-  count: ICountState;
-};
-
 export default combineReducers({
   user,
   todo,
-  style,
-  count: countReducer
+  style
 });
